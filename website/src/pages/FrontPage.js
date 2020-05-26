908a5cf9adbb69a1920f80918116ec3c3912961d
dwipatel <dwipatel00@gmail.com>
2020-05-25 21:38:18 -0400

    Frontpage complete

---
+++ website/src/pages/FrontPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Nav from '../components/navbar.component';
+import FrontImage from '../front.jpg';
+
+import '../styles/frontPage.css';
+
+
+export default class FrontPage extends Component {
+    render() {
+        return (
+            <div>
+                <Nav/>
+                <div className="front-page">
+                    <p className="my-name">Dwip Patel.</p>
+                    <p className="my-program">McMaster University, Electrical Engineering Level II</p>
+                    
+                    {/* <div className="front-desc">
+                        <p className="my-name">Dwip Patel.</p>
+                        <p className="my-program">McMaster University, Electrical Engineering Level II</p>
+                    </div> */}
+                </div>
+                <div className="pic-div">
+                    <img className="front-page-pic" src={FrontImage} alt="Front Picture" width="460" height="345"/>
+                </div>
+                
+            </div>
+            
+        )
+    }
+}
