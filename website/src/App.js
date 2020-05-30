9ac440cdd2fbc985190b0d9453c69d54ffbe4114
dwipatel <dwipatel00@gmail.com>
2020-05-30 16:35:25 -0400

    Set up react router

--- website/src/App.js
+++ website/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-//import Navigation from './components/navbar';
+import Navigation from './components/navbar.component';
+import FrontPage from './pages/FrontPage';
+import ProjectPage from './pages/ProjectPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return(
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={FrontPage}/>
+          <Route path="/projects" component={ProjectPage}/>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Navigation/>
+//       </header>
+//     </div>
+//   );
+// }
+
+//export default App;
