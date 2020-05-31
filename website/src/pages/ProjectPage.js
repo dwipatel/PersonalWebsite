8d25cac6c9a60d99bbc700a3a304104436e3e0cc
dwipatel <dwipatel00@gmail.com>
2020-05-31 14:19:23 -0400

    Added Header

--- website/src/pages/ProjectPage.js
+++ website/src/pages/ProjectPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Project from '../components/project.component';
 import Nav from '../components/navbar.component';
+import Header from '../components/header';
 
 import '../styles/projectPage.css';
 
@@ -10,8 +11,11 @@ export default class Projects extends Component {
             <div>
                 <Nav/>
                 <div className="projects">
+                    <Header title="Projects"/>
                     <Project name="BiLLy" description="Made at RUHacksousVBvdisdbavapibSVOJIPSD VOJBS FEDU JFUEHHEFHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH J EO J EJ JFVVDVSA VASDV ASDVADSV ASVDASDVA SDVAVASDVASDV ADSVADSVA DVADSVASD VADSVA SDVADSVASD VADSV ADSV ADSVADSV ADS VASD VADSV ADS VADSV"/>
-                    <Project name="BiLLy2" description="Made at RUHackssss"/>
+                    <Project name="BiLLy2" description="Made at RUHackssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"/>
+                    <Project name="BiLLy2" description="Made at RUHackssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"/>
+                    <Project name="BiLLy2" description="Made at RUHackssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"/>
                 </div>
             </div>
         )
