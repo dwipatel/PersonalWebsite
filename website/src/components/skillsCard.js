4191649f5490f562f9a049ccf7521b47cbbf6c66
dwipatel <dwipatel00@gmail.com>
2020-06-01 16:40:20 -0400

    Updated skills

--- website/src/components/skillsCard.js
+++ website/src/components/skillsCard.js
@@ -1,56 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import '../styles/skillsCard.css';
 
 export default class SkillsCard extends Component {
     render() {
         return(
-            <div className="skills-card">
-                
-                <div className="language">
-                    <h3 className="language-title">Languages</h3>
-                    <p className="language-item">Java</p>
-                    <p className="language-item">Python</p>
-                    <p className="language-item">HTML/CSS</p>
-                    <p className="language-item">C</p>
-                    <p className="language-item">JavaScript</p>
-                    <p className="language-item">Verilog</p>
+            <div>
+                <div className="skill-title">
+                    <p>Skills</p>
                 </div>
+                <div className="skills-card">
+                    <div className="big-div">
+                        <p className="item-title">Languages</p>
+                        <p className="item">Java</p>
+                        <p className="item">Python</p>
+                        <p className="item">HTML</p>
+                        <p className="item">CSS</p>
+                        <p className="item">C</p>
+                        <p className="item">JavaScript</p>
+                        <p className="item">Verilog</p>
+                    </div>
 
-                <div className="technology">
-                    <h3 className="technology-title">Technologies</h3>
-                    <p className="technology-item">React.js</p>
-                    <p className="technology-item">Node.js</p>
-                    <p className="technology-item">Express.js</p>
-                    <p className="technology-item">MongoDB</p>
-                    <p className="technology-item">AutoCAD</p>
-                    <p className="technology-item">Git</p>
-                    <p className="technology-item">React Native</p>
+                    <div className="big-div">
+                        <p className="item-title">Technologies</p>
+                        <p className="item">React.js</p>
+                        <p className="item">Node.js</p>
+                        <p className="item">Express.js</p>
+                        <p className="item">MongoDB</p>
+                        <p className="item">AutoCAD</p>
+                        <p className="item">Git</p>
+                        <p className="item">React Native</p>
+                    </div>
                 </div>
-
-
-                {/* <ul className="language">
-                    <h3 className="language-title">Languages</h3>
-                    <li className="language-item">Java</li>
-                    <li className="language-item">Python</li>
-                    <li className="language-item">HTML/CSS</li>
-                    <li className="language-item">C</li>
-                    <li className="language-item">JavaScript</li>
-                    <li className="language-item">Verilog</li>
-                </ul>
-
-                <ul className="technology">
-                    <h3 className="technology-title">Technologies</h3>
-                    <li className="technology-item">React.js</li>
-                    <li className="technology-item">Node.js</li>
-                    <li className="technology-item">Express.js</li>
-                    <li className="technology-item">MongoDB</li>
-                    <li className="technology-item">AutoCAD</li>
-                    <li className="technology-item">Git</li>
-                    <li className="technology-item">React Native</li>
-                </ul> */}
-
             </div>
+            
         )
     }
 }
