b56a4590c7d0806843dc145856c20e1af1f804d8
dwipatel <dwipatel00@gmail.com>
2020-06-01 16:41:15 -0400

    Added skills card

--- website/src/pages/FrontPage.js
+++ website/src/pages/FrontPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Nav from '../components/navbar.component';
 import FrontImage from '../front.jpg';
 import SkillCard from '../components/skillsCard.js';
+import Header from '../components/header';
 
 import '../styles/frontPage.css';
 
@@ -12,21 +13,18 @@ export default class FrontPage extends Component {
             <div>
                 <Nav/>
                 <div className="front-page">
-                    <p className="my-name">Dwip Patel.</p>
-                    <p className="my-program">McMaster University, Electrical Engineering Level II</p>
-                    
-                    {/* <div className="front-desc">
+                    <Header title="Home"/>
+                    <div className="text">
                         <p className="my-name">Dwip Patel.</p>
-                        <p className="my-program">McMaster University, Electrical Engineering Level II</p>
-                    </div> */}
-                </div>
-                {/* <div className="pic-div">
-                    <img className="front-page-pic" src={FrontImage} alt="Front Picture" />
-                </div> */}
-                <div className="pic-div">
-                    <SkillCard />
-                </div>
-                
+                        <p className="my-program">McMaster University, Electrical Engineering Level III</p>
+                    </div>
+                    <div className="skill">
+                        <SkillCard/>
+                    </div>
+                    
+                    
+                   
+                </div>               
             </div>
             
         )
