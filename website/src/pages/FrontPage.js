17d96a36f62bb7893ea04249f5691839a47e17e6
dwipatel <dwipatel00@gmail.com>
2020-06-08 01:45:28 -0400

    Added skills card

--- website/src/pages/FrontPage.js
+++ website/src/pages/FrontPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Nav from '../components/navbar.component';
 import FrontImage from '../front.jpg';
-import SkillCard from '../components/skillsCard.js';
+import SkillCard from '../components/skillsCard';
+import SkillCard2 from '../components/skillsCard2';
 import Header from '../components/header';
 import Experience from '../components/experience';
 import Logo from '../assets/Logo.png';
@@ -15,9 +16,43 @@ export default class FrontPage extends Component {
             <div className="front-page">
                 <div className="front-home">
                     <img src={Logo} alt="Site Logo" className="front-logo"/>
-                    <p className="my-name">Hi, I'm Dwip Patel</p>
+                    <div className="my-name">
+                        <p className="my-name-1">Hi, I'm</p>
+                        <p className="my-name-2">Dwip Patel</p>
+                    </div>
+                    
                 </div>
                 <Nav/>
+                <div className="skill-card">
+                    <div >
+                        <p className="skill-title">Skills</p>
+                    </div>
+                    <div className="skill-list">
+                        <div className="lang-block">
+                            <h3 className="lang-title">Languages</h3>
+                            <SkillCard lang="Java"/>
+                            <SkillCard lang="Python"/>
+                            <SkillCard lang="HTML"/>
+                            <SkillCard lang="CSS"/>
+                            <SkillCard lang="C"/>
+                            <SkillCard lang="JavaScript"/>
+                            <SkillCard lang="Verilog"/>
+                        </div>
+                        <div className="lang-block">
+                            <h3 className="lang-title">Technologies</h3>
+                            <SkillCard2 tech="React.js"/>
+                            <SkillCard2 tech="Node.js"/>
+                            <SkillCard2 tech="Express.js"/>
+                            <SkillCard2 tech="MongoDB"/>
+                            <SkillCard2 tech="AutoCAD"/>
+                            <SkillCard2 tech="Git"/>
+                            <SkillCard2 tech="React_Native"/>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+                
             </div>
         )
     }
